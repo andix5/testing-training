0c8e46404bc69e5bc6b635d40b82f5a0b62ae3b7
pe22gug <anderson.mendes@eduvaud.ch>
2025-12-09 13:33:35 +0100

    feat: all getters and setters

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -8,61 +8,90 @@ module.exports = class CartItem {
 
     //region private attributes
     //TODO Missing private attributs
+    #_articleId;
+    #_name;
+    #_quantity;
+    #_price;
     //endregion private attributes
 
     //region public methods
     constructor(articleId, name, quantity, price) {
         //TODO Implement this method
+        this.#articleId = articleId;
+        this.#name = name;
+        this.quantity = quantity;
+        this.price = price;
     }
 
     get articleId() {
         //TODO Implement this method
+        return this.#_articleId;
     }
 
     get name() {
         //TODO Implement this method
+        return this.#_name;
     }
 
     get quantity() {
         //TODO Implement this method
+        return this.#_quantity;
     }
 
     set quantity(value) {
         //TODO Implement this method
+        this.#_quantity = this.#validateQuantity(value);
     }
 
     get price() {
         //TODO Implement this method
+        return this.#_price;
     }
 
     set price(value) {
         //TODO Implement this method
+        this.#_price = this.#validatePrice(value);
     }
 
     get total() {
         //TODO Implement this method
+        return this.#_quantity * this.#_price;
     }
     //endregion public methods
 
     //region private methods
     set #articleId(value) {
         //TODO Implement this method
+        this.#_articleId = this.#validateArticleId(value);
     }
 
     set #name(value) {
         //TODO Implement this method
+        this.#_name = value;
     }
 
     #validateArticleId(articleId) {
         //TODO Implement this method
+        if (!Number.isInteger(articleId) || articleId <= 0) {
+            throw new InvalidArticleIdException();
+        }
+        return articleId;
     }
 
     #validateQuantity(quantity) {
         //TODO Implement this method
+        if (!Number.isInteger(quantity) || quantity < 1) {
+            throw new InvalidQuantityException();
+        }
+        return quantity;
     }
 
     #validatePrice(price) {
         //TODO Implement this method
+        if (typeof price !== "number" || price < 10) {
+            throw new InvalidPriceException();
+        }
+        return price;
     }
     //endregion private methods
 }
